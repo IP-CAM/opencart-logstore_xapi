f5f99debdbdb49bfeb97a9e91a84fa9be91dd87d
Andy Hubert <andy@not-a-real-domain.com>
2018-09-30 18:07:01 +0300

    Fixed order options

--- upload/catalog/controller/extension/module/logstore_xapi/utils/get_order_options.php
+++ upload/catalog/controller/extension/module/logstore_xapi/utils/get_order_options.php
@@ -1,17 +1,19 @@
 <?php
   function get_order_options($order_row, $order_product_row, $general) {
 
-    $product_condition = "povtmp.product_id='" . $general['db']->escape($order_product_row['product_id']) . "'";
+    $product_condition_minus_table = ".product_id='" . $general['db']->escape($order_product_row['product_id']) . "'";
 
     // get order options from the DB
     $order_option_rows = $general['db']->query(
-      "SELECT oo.product_option_id, oo.product_option_value_id, oo.name, oo.value, povtmp.unit, povtmp.interval " .
+      "SELECT po.option_id, pov.option_value_id, oo.name, oo.value, povtmp.unit, povtmp.interval " .
       "FROM `" . DB_PREFIX . "order_option` as oo " .
-      // "LEFT JOIN `" . DB_PREFIX . "product_option_value` as pov " .
-      //   "ON (pov.product_option_value_id=oo.product_option_value_id) " .
+      "LEFT JOIN `" . DB_PREFIX . "product_option` as po " .
+        "ON (po.product_option_id=oo.product_option_id AND po" . $product_condition_minus_table . ") " .
+      "LEFT JOIN `" . DB_PREFIX . "product_option_value` as pov " .
+        "ON (pov.product_option_value_id=oo.product_option_value_id AND pov" . $product_condition_minus_table . ") " .
       "LEFT JOIN `" . DB_PREFIX . "product_option_value_to_moodle_period` as povtmp " .
-        "ON (povtmp.product_option_value_id=oo.product_option_value_id AND " . $product_condition . ") " .
-      "WHERE oo.order_id='" . $general['db']->escape($order_row['order_id']) . "' AND " . $product_condition
+        "ON (povtmp.product_option_value_id=oo.product_option_value_id AND povtmp" . $product_condition_minus_table . ") " .
+      "WHERE oo.order_id='" . $general['db']->escape($order_row['order_id']) . "' AND povtmp" . $product_condition_minus_table
     )->rows;
 
 
@@ -31,12 +33,13 @@
           break;
       }
 
-      $optionType = $order_option_row['name'] . ' [' . $order_option_row['product_option_id'] . ']';
-      $optionValue = $order_option_row['value'] . ' [' . $order_option_row['product_option_value_id'] . ']';
+      $optionType = $order_option_row['name'] . ' [id:' . $order_option_row['option_id'] . ']';
+      $optionValue = $order_option_row['value'] . ' [id:' . $order_option_row['option_value_id'] . ']';
 
       $options[] = array_merge(
         [
-          $optionType => $optionValue,
+          "name" => $optionType,
+          "value" => $optionValue,
         ],
         isset($expiretime)
           ? [
